69a6ced3b537ccdcaef33d28bdfd7a7f26e9c49e
keomaklein <keoma.klein1@gmail.com>
2021-01-29 19:46:18 -0300

    Adicionado configuração para gateway nos microservicos

--- gateway/src/main/java/com/keomaklein/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/keomaklein/gateway/GatewayApplication.java
@@ -2,8 +2,14 @@ package com.keomaklein.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import jdk.jfr.Enabled;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class GatewayApplication {
 
 	public static void main(String[] args) {
